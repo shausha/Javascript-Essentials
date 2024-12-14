cf7fd8dc594e1353d4827c9a3652b51ac1e08da2
shausha <ushajawahar23@gmail.com>
2024-12-14 20:46:36 +0530

    [UJ] day 3

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-//console.log(`Hello`);
+//console.log(`Hello`); / print statement in console
 
-//window.alert(`This is alert`);
+//window.alert(`This is alert`); / alert statement
 
-//document.getElementById("mine").textContent=`Hello`;
+//document.getElementById("mine").textContent=`Hello`; / connect html and js using id
 //document.getElementById("para").textContent=`My name is usha`;
 //comment
 
@@ -62,14 +62,43 @@ document.getElementById("p3").textContent = `Am I student? ${student}`;
 Example: 11 = x+5;
 */
 
-let student= 60;
-
-student= student+1;
-student= student-1;
-student= student*2;
-student= student/2;
-student= student**2;
-student= student**3;
-let extrastudent= student % 7 ;
-console.log(student);
+//let student= 60;
+/* EXAMPLE
+student= student+1; // add
+student= student-1; // subract
+student= student*2; // multiply
+student= student/2; /divide
+student= student**2; //exponent
+student= student**3; // exponent
+let extrastudent= student % 7 ; // modulo- to deteremine a number is even or odd
 console.log(extrastudent);
+*/
+
+//AUGMENTED ASSIGNMENT OPERATOR
+/* EXAMPLE
+student+=1;
+student-=1;
+student*=2;
+student/=2;
+student**=3;
+student%=2;
+*/
+//INCREMENT and DECREMENT OPERATOR
+/* EXAMPLE
+student ++; // increment
+student --; // decrement
+console.log(student);
+*/
+
+// OPERATOR PRECEDENCE - Order to solve mathematical expression from left to right
+/*
+    1. Parenthesis ()
+    2. exponents
+    3. multiplication & division & modulo
+    4. addition & subraction
+*/
+/* EXAMPLE
+let result = 1 + 2 * 3 + 4 ** 2;
+
+console.log(result);
+*/
